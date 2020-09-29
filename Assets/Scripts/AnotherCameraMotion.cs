46f269854aa88414f46b513826190f7087468771
hilaperi <44542368+hilaperi@users.noreply.github.com>
2020-09-29 09:35:09 +0300

    t
    t

---
+++ Assets/Scripts/AnotherCameraMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherCameraMotion : MonoBehaviour
+{
+    float angularSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angularSpeed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * angularSpeed, 0);
+    }
+}
--- Assets/Scripts/RedHumanMotion.cs
+++ Assets/Scripts/RedHumanMotion.cs
@@ -13,7 +13,7 @@ public class RedHumanMotion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5f;
+        speed = 7f;
         angularSpeed = 100f;
         cController = GetComponent<CharacterController>();
        // audioSource = GetComponent<AudioSource>();
@@ -30,16 +30,16 @@ public class RedHumanMotion : MonoBehaviour
             vMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
             GetComponent<Animation>().Play("ShieldWarrior@Walk01");
             transform.Rotate(0, hMove, 0);
-            // pos is the x-z coordinate of a character
-            // Vector3 pos = new Vector3(transform.position.x, 0, transform.position.z);
-            // Vector3 point;
-            //  point.y = Terrain.activeTerrain.SampleHeight(pos) - transform.position.y; // delta in Y direction
-            //  Vector3 direction = Vector3.forward * vMove;
-            //  direction.y = point.y;
 
-            //transform.Translate(Vector3.forward * vMove);
+            //pos is the x-z coordinate of a character
+            //For the terrain 
+            Vector3 pos = new Vector3(transform.position.x, 0, transform.position.z);
+           // Vector3 point;
+           // point.y = Terrain.activeTerrain.SampleHeight(pos) - transform.position.y; // delta in Y direction
+            Vector3 direction = Vector3.forward * vMove;
+           // direction.y = point.y;
             // TransformDirection transforms coordinates to GLOBALs
-            cController.Move(transform.TransformDirection(Vector3.forward * vMove));
+            cController.Move(transform.TransformDirection(direction));
 
             // sound effect
             //  if (!audioSource.isPlaying)
